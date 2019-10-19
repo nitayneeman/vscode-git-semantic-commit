830b9eb89fbd3c881693eca6d7d5d6f398dc9609
nitayneeman <nitayneeman@users.noreply.github.com>
2019-10-19 20:33:46 +0300

    chore: improve common test file

--- src/git.ts
+++ src/git.ts
@@ -1,45 +1,30 @@
-import { workspace } from 'vscode';
-
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
+import { getWorkspaceFolder } from './workspace';
+
 export class Git {
   static exists() {
-    return this.execute(this.getWorkspaceFolder(), '', ['--version']);
-  }
-
-  static async init(cwd: string) {
-    return this.execute(cwd, 'init', ['--quiet']);
-  }
-
-  static async status(cwd: string, options: string[] = []) {
-    return this.execute(cwd, 'status', [...options]);
+    return this.execute(getWorkspaceFolder(), '', ['--version']);
   }
 
   static async add() {
-    const { stdout: hasStaged } = await this.status(this.getWorkspaceFolder(), [
-      '--porcelain',
-      '--untracked-files=no'
-    ]);
+    const { stdout: hasStaged } = await this.status(['--porcelain', '--untracked-files=no']);
 
     if (!hasStaged) {
-      return this.execute(this.getWorkspaceFolder(), 'add', [`--all`]);
+      return this.execute(getWorkspaceFolder(), 'add', [`--all`]);
     }
   }
 
   static async commit(message: string, options: string[]) {
-    return this.execute(this.getWorkspaceFolder(), 'commit', [
-      `--message="${message}"`,
-      ...options
-    ]);
+    return this.execute(getWorkspaceFolder(), 'commit', [`--message="${message}"`, ...options]);
   }
 
-  private static execute(cwd: string, command?: string, options: string[] = []) {
+  static execute(cwd: string, command?: string, options: string[] = []) {
     return exec(`git ${command} ${options.join(' ')}`, { cwd });
   }
 
-  private static getWorkspaceFolder(): string {
-    const { workspaceFolders } = workspace;
-    return workspaceFolders ? workspaceFolders[0].uri.fsPath : '';
+  private static async status(options: string[] = []) {
+    return this.execute(getWorkspaceFolder(), 'status', [...options]);
   }
 }
--- src/test/suite/common.ts
+++ src/test/suite/common.ts
@@ -1,37 +1,28 @@
 import * as fs from 'fs';
-import * as os from 'os';
 import * as path from 'path';
 const fsExtra = require('fs-extra');
 
-import { extensionIdentifier } from '../../constants';
 import { Git } from '../../git';
 
-function getRandomName() {
+function generateRandomName() {
   return Math.random()
     .toString(36)
     .replace(/[^a-z]+/g, '')
     .substr(0, 10);
 }
 
-export async function createGitDirectory() {
-  const directory = os.tmpdir();
-  const directoryPath = fs.mkdtempSync(path.join(directory, `${extensionIdentifier}-`));
-
-  await Git.init(directoryPath);
-
-  return directoryPath;
-}
-
 export async function createFile(directoryPath: string, content = '', extension = 'txt') {
-  const filePath = path.join(directoryPath, `${getRandomName()}.${extension}`);
+  const filePath = path.join(directoryPath, `${generateRandomName()}.${extension}`);
 
-  return fs.writeFileSync(filePath, content);
+  fs.writeFileSync(filePath, content);
+
+  return filePath;
 }
 
-export async function checkStatus(directoryPath: string) {
-  return await Git.status(directoryPath);
+export async function getLastMessage(directoryPath: string) {
+  return await Git.execute(directoryPath, 'log', ['-1', '--pretty=%B']);
 }
 
-export async function removeDirectory(directoryPath: string) {
-  return fsExtra.remove(directoryPath);
+export async function clearDirectory(directoryPath: string) {
+  return fsExtra.emptyDir(directoryPath);
 }
