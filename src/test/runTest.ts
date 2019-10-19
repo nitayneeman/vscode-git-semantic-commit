fe10018923ca2d96ce1aad3a51affb746768769a
nitayneeman <nitayneeman@users.noreply.github.com>
2019-10-19 20:45:51 +0300

    chore: add test runner args

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -6,7 +6,11 @@ async function main() {
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
     const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
-    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs: ['${workspaceFolder}/test-temp', '--disable-extensions']
+    });
   } catch (err) {
     console.error('Failed to run tests');
     process.exit(1);
