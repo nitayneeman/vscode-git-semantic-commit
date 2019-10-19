ee24f80213d27427b7fe748224973c5747339af4
nitayneeman <nitayneeman@users.noreply.github.com>
2019-10-19 11:29:28 +0300

    chore: improve common test file

--- src/test/suite/common.ts
+++ src/test/suite/common.ts
@@ -14,6 +14,6 @@ export async function createGitDirectory() {
   return dirPath;
 }
 
-export async function removeGitDirectory(dirPath: string) {
+export async function removeDirectory(dirPath: string) {
   return fs.rmdirSync(dirPath);
 }
