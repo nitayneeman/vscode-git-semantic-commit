15716aded2b355f1e6cc0669bea14828884b5489
Nitay Neeman <nitayneeman@users.noreply.github.com>
2019-10-19 21:13:48 +0300

    chore: change Travis to run test (#1)

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -5,8 +5,13 @@ async function main() {
   try {
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
     const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    const testWorkspace = path.resolve(__dirname, './test-temp');
 
-    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs: [testWorkspace]
+    });
   } catch (err) {
     console.error('Failed to run tests');
     process.exit(1);
