f262528f527ff5a430a77d467dcdac9107846562
Max P <Mail@MPassarello.de>
2024-08-24 01:30:10 +0200

    feat: Add support for multi-line commit messages

--- src/git.ts
+++ src/git.ts
@@ -1,30 +1,35 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
-import { getWorkspaceFolder } from './workspace';
+import { getWorkspaceFolder } from "./workspace";
 
 export class Git {
   static exists() {
-    return this.execute(getWorkspaceFolder(), '', ['--version']);
+    return this.execute(getWorkspaceFolder(), "", ["--version"]);
   }
 
   static async add() {
-    const { stdout: changes } = await this.diff(['--cached']);
+    const { stdout: changes } = await this.diff(["--cached"]);
 
     if (changes.length === 0) {
-      return this.execute(getWorkspaceFolder(), 'add', [`--all`]);
+      return this.execute(getWorkspaceFolder(), "add", [`--all`]);
     }
   }
 
   static async commit(message: string, options: string[]) {
-    return this.execute(getWorkspaceFolder(), 'commit', [`--message="${message}"`, ...options]);
+    const messageOptions = message.split("\n").map((line) => `-m "${line}"`);
+
+    return this.execute(getWorkspaceFolder(), "commit", [
+      ...messageOptions,
+      ...options,
+    ]);
   }
 
   static execute(cwd: string, command?: string, options: string[] = []) {
-    return exec(`git ${command} ${options.join(' ')}`, { cwd });
+    return exec(`git ${command} ${options.join(" ")}`, { cwd });
   }
 
   private static async diff(options: string[] = []) {
-    return this.execute(getWorkspaceFolder(), 'diff', [...options]);
+    return this.execute(getWorkspaceFolder(), "diff", [...options]);
   }
 }
